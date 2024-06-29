0f429f962099aebce5e7c9004ae40b53fc5627f2
Bordo <joan.bordonaba@gmail.com>
2024-06-29 14:57:08 +0200

    cors r, response

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express, { Router } from "express";
 import { router } from "./router.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
--- server/src/router.js
+++ server/src/router.js
@@ -6,10 +6,27 @@ const upload = multer({ dest: "./public/uploads/video" });
 
 router.post("/uploadVideo", upload.single("video"), async (req, res) => {
   const video = req.file;
+
   const response = await fetch("processVideo", {});
+  const dataVideo = await response.json();
+
   const response1 = await fetch("transcriveAudio");
+  const dataTansf = await response.json();
+
   const respons2 = await fetch("comperTo");
+  const dataCompare = await response.json();
+
   const respons3 = await fetch("analizeEmotion");
+  const dataEmotion = await response.json();
+
   const respons4 = await fetch("analizeBodyLL");
-  // res.send();
+  const dataBody = await response.json();
+
+  res.send({
+    video: dataVideo,
+    transcrif: dataTansf,
+    compare: dataCompare,
+    emotion: dataEmotion,
+    bodyLL: dataBody,
+  });
 });
