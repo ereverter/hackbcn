34c9a1a64ea1cd26bc91b36f5a713a0a16a860b5
Bordo <joan.bordonaba@gmail.com>
2024-06-30 11:35:08 +0200

    aslasd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,33 +27,33 @@ function App() {
   const [uploaded, setUploaded] = useState(false);
   console.log(dataFetch);
   const objetFromFetch = [];
-  let data = {};
-  // if (dataFetch) {
-  //   dataFetch.map((item) => objetFromFetch.push(item));
-  //   dataOBJ = {
-  //     labels: [
-  //       "Admiration",
-  //       "Anxiety",
-  //       "Boredom",
-  //       "Calmness",
-  //       "Confusion",
-  //       "Dussapointment",
-  //       "Doubt",
-  //       "Excitement",
-  //       "Interest",
-  //       "Joy",
-  //     ],
-  //     datasets: [
-  //       {
-  //         label: "# of Votes",
-  //         data: objetFromFetch,
-  //         backgroundColor: "rgba(255, 99, 132, 0.2)",
-  //         borderColor: "rgba(255, 99, 132, 1)",
-  //         borderWidth: 10,
-  //       },
-  //     ],
-  //   };
-  // }
+  // let data = {};
+  if (dataFetch) {
+    dataFetch.map((item) => objetFromFetch.push(item));
+  }
+  const data = {
+    labels: [
+      "Admiration",
+      "Anxiety",
+      "Boredom",
+      "Calmness",
+      "Confusion",
+      "Dussapointment",
+      "Doubt",
+      "Excitement",
+      "Interest",
+      "Joy",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: objetFromFetch,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 10,
+      },
+    ],
+  };
 
   const processEmotionSums = (grouped_transcription) => {
     const emotionSums = {};
@@ -131,6 +131,7 @@ function App() {
     })
       .then(async (result) => {
         const data = await result.json();
+        console.log("data from fetch:", data);
         setDatafetch(data.data);
       })
       .finally(() => {
@@ -273,6 +274,9 @@ function App() {
               </div>
             )}
           </section>
+          <div>
+            <p>{dataFetch.evaluation}</p>
+          </div>
         </div>
       </main>
     </>
