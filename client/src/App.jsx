d96b252d51ab31bd20648ddccbb4fe13366f3f2f
avila-ca <avilacalaf@gmail.com>
2024-06-30 11:32:07 +0200

    preparing to recieve the response server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -58,14 +58,16 @@ function App() {
       body: formData,
     })
     .then(async (result) =>{     
-      const data = await result.json();
-      console.log(data)
-      if (data && data.grouped_transcription) {
-        setDatafetch(data.grouped_transcription);
-        setUploaded(true);
-      } else {
-        console.error('Invalid data structure:', data);
-      }
+      //const data = await result.json();
+      console.log(dataExample)
+      setDatafetch(dataExample.grouped_transcription);
+      setUploaded(true);
+      setText(data.llmText)
+      setText(data.userText)
+      // if (data && data.grouped_transcription) {
+      // } else {
+      //   console.error('Invalid data structure:', data);
+      // }
     })
     .catch(error => {
       console.error('Error:', error);
@@ -102,7 +104,7 @@ function App() {
       ],
       datasets: [
         {
-          label: "Emotion Scores",
+          label: "Emotion Scores Stastics",
           data: emotionSums,
           backgroundColor: "rgba(255, 99, 132, 0.2)",
           borderColor: "rgba(255, 99, 132, 1)",
@@ -179,34 +181,7 @@ function App() {
                       transcript
                     </h5>
                     <p className="font-normal text-justify py-3 text-gray-700 dark:text-gray-400">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      At qui nemo consequatur totam provident voluptatem soluta
-                      labore deleniti dolor pariatur eaque maiores dolorem,
-                      asperiores natus animi perspiciatis explicabo, fugit
-                      laudantium ducimus rem sequi! Ipsum excepturi atque vero
-                      quia, ab officiis sint corrupti aliquam dolorem eos
-                      asperiores libero sed laudantium possimus explicabo
-                      delectus veniam quisquam exercitationem similique dolorum
-                      tenetur qui doloremque. Eligendi quidem aperiam
-                      accusantium quae harum dolores dolorum cumque deleniti
-                      excepturi, nobis assumenda unde deserunt facilis. Amet
-                      reprehenderit ut dignissimos perferendis natus. Iure,
-                      nostrum voluptate molestias odio dolores commodi ad porro
-                      placeat aliquam id quos ab a aspernatur expedita iste?
-                      Reprehenderit recusandae neque officia? Expedita, dolores
-                      officiis suscipit nostrum eos porro assumenda cum sunt
-                      earum quaerat deserunt dicta optio consequuntur est, sint
-                      adipisci accusamus similique? Quas dignissimos ad esse
-                      fugit aperiam sequi maiores. Omnis dolor accusantium iure,
-                      exercitationem a mollitia illo vitae, neque ducimus
-                      perferendis ut! Aperiam magnam nostrum ipsa laborum.
-                      Voluptas facilis harum molestias consequatur in deleniti
-                      recusandae assumenda optio delectus odio unde repellat ut,
-                      cum, dolorem nisi sunt odit blanditiis voluptate? Eius
-                      quasi excepturi quod facere? Commodi, similique? Minima
-                      reiciendis quis adipisci, vitae fugit iste officiis
-                      mollitia. Optio aperiam possimus neque, rerum hic
-                      reiciendis! Cumque, nulla. Cupiditate, tempore!
+                      {text.userText}
                     </p>
                   </article>
                   <article className="max-w-sm m-5">
@@ -214,33 +189,7 @@ function App() {
                       <span className="text-red-500">LLM trancript</span>
                     </h5>
                     <p className="font-normal text-justify py-3 text-gray-700 dark:text-gray-400">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Porro, ullam, iure temporibus deleniti et sit iste quas
-                      soluta cum eligendi molestiae facere officia sunt
-                      accusamus quia repellendus labore voluptas tenetur? Ipsa,
-                      facere aut. Vitae ipsum minima placeat dolorum error
-                      obcaecati unde iusto ullam officiis nemo. Sed quisquam
-                      voluptatibus facilis debitis laudantium exercitationem
-                      omnis consequatur esse, neque laboriosam ducimus repellat,
-                      odio quo id nisi quis repellendus nobis ut? Magnam in
-                      delectus atque illo ex, vero rerum dolor officia tempora
-                      consequatur quisquam ducimus, ratione sequi, iste dicta
-                      asperiores quidem iure praesentium ab repudiandae vitae
-                      ipsa corrupti? At molestiae, mollitia nihil suscipit
-                      expedita fugiat sequi quo, minus vitae vel facilis
-                      voluptatibus maiores quos commodi dolores quod! Ut vel
-                      doloremque error beatae minus ipsum labore consequuntur
-                      alias est amet voluptas maiores officia libero, adipisci
-                      eligendi! Eveniet cupiditate maiores, magnam, recusandae
-                      porro molestiae quia dolorem provident eligendi adipisci
-                      dicta totam. Accusamus culpa, alias excepturi labore sint
-                      nostrum aperiam dicta dolorum dolore provident blanditiis!
-                      Dicta ducimus odit sunt tempora velit? Exercitationem
-                      eligendi voluptatibus blanditiis itaque dolore accusamus
-                      obcaecati? Repellat recusandae quam rem ratione et magnam
-                      corporis autem quas nobis cum magni aspernatur, corrupti
-                      dolore temporibus harum, consequuntur quasi sint, quidem
-                      iusto dignissimos veniam eos itaque soluta!
+                     {text.llmText}
                     </p>
                   </article>
                 </div>
